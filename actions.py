4efebd38445c27ca812b99d2020b19fb2af54a0e
Vulpie <wojtas_f@outlook.com>
2020-08-18 00:02:33 +0200

    feat: initial commit

---
+++ actions.py
@@ -0,0 +1,75 @@
+from typing import Any, Text, Dict, List
+from sqlalchemy import create_engine, Table
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+Base = declarative_base()
+engine = create_engine("sqlite:///menu_list.db", echo=True)
+
+class PizzaList(Base):
+    __table__ = Table('pizza_type', Base.metadata, autoload=True, autoload_with=engine)
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class ActionHelloWorld(Action):
+    def name(self) -> Text:
+        return "action_facility_search"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        facility = tracker.get_slot("facility_type")
+        address = "Hetmańska 99/45"
+
+        dispatcher.utter_message(
+            "Here is the address of the {}:{}".format(facility, address)
+        )
+
+        return [SlotSet("address", address)]
+
+
+class ActionSaveOrder(Action):
+    def name(self) -> Text:
+        return "action_save_order"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        pizza_type = tracker.get_slot("pizza_type")
+        pizza_size = tracker.get_slot("pizza_size")
+
+        dispatcher.utter_message("Order summary: {} {}".format(pizza_size, pizza_type))
+
+        return []
+
+
+class ActionGetMenuList(Action):
+    def name(self) -> Text:
+        return "action_get_menu"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        
+        menu = session.query(PizzaList).all()
+        print(menu)
+
+        dispatcher.utter_message("Menu: {}".format(menu))
+        session.close()
+        return []
---
+++ data/nlu.md
@@ -0,0 +1,61 @@
+## intent:greet
+-   hey
+-   hello
+-   hi
+-   good morning
+-   good evening
+-   hey there
+
+## intent:goodbye
+-   i have to go
+-   take care
+-   bye
+-   goodbye
+-   see you
+-   gotta go
+-   i'm out
+
+## intent:about_me
+-   who are you?
+-   are you a bot?
+-   you bot?
+-   are you a human?
+
+## intent:inform
+- [Rzeszów](location)
+- [Kraków](location)
+- [Warszawa](location)
+- [Stalowa Wola](location)
+- [Nisko](location)
+
+## intent:search_provider
+- I'm looking for a [restaurant](facility_type)
+- I want to eat something in a [restaurant](facility_type)
+- Let's eat something
+- Lets grab something to eat
+- where can I eat something
+
+## intent:menu
+- i want to see your menu
+- what can I eat here?
+- I'm hungry
+- I'm starving
+- I'm famished
+- Show me what I can eat here
+
+## intent:order
+- i want to order [large](pizza-size) [americana](pizza_type)
+- I want [small](pizza-size) [italiana](pizza_type) pizza
+- Can I get [medium](pizza_size) [margherita](pizza_type)
+- I want to order pizza
+- Can I get pizza
+
+## intent:get_size
+- i want [large](pizza_size) pizza
+- [medium](pizza_size)
+- make it [small](pizza_size)
+
+## intent:get_type
+- I want [americana](pizza_type)
+- Can i order [mergherita](pizza_type)
+- Get me [frutti di mare](pizza_type)
